aad063e41870d7b43bf6d513bb597ed70a523b80
Barry Yap <barry_yap@aiap.sg>
2022-08-16 20:27:32 +0800

    added code for deploying to polygon mumbai subchain

--- contracts/WEB3RSVP.sol
+++ contracts/WEB3RSVP.sol
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.9;
 
 contract Web3RSVP {
@@ -51,7 +52,7 @@ contract Web3RSVP {
         address[] memory confirmedRSVPs;
         address[] memory claimedRSVPs;
 
-        // this creates a new CreateEvent struct and adds it to the idToEvent mapping
+        //this creates a new CreateEvent struct and adds it to the idToEvent mapping
         idToEvent[eventId] = CreateEvent(
             eventId,
             eventDataCID,
@@ -75,22 +76,22 @@ contract Web3RSVP {
     }
 
     function createNewRSVP(bytes32 eventId) external payable {
-        // look up event from our mapping
+        // look up event
         CreateEvent storage myEvent = idToEvent[eventId];
 
-        // transfer deposit to our contract / require that they send in enough ETH to cover the deposit
+        // transfer deposit to our contract / require that they sent in enough ETH
         require(msg.value == myEvent.deposit, "NOT ENOUGH");
 
-        // require that the event hasn't already happened (<eventTimestamp)
+        //require that the event hasn't already happened (<eventTimestamp)
         require(block.timestamp <= myEvent.eventTimestamp, "ALREADY HAPPENED");
 
-        // make sure event is under max capacity
+        //make sure event is under max capacity
         require(
             myEvent.confirmedRSVPs.length < myEvent.maxCapacity,
             "This event has reached capacity"
         );
 
-        // require that msg.sender isn't already in myEvent.confirmedRSVPs AKA hasn't already RSVP'd
+        //require that msg.sender isn't already in myEvent.confirmedRSVPs
         for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
             require(
                 myEvent.confirmedRSVPs[i] != msg.sender,
@@ -103,17 +104,30 @@ contract Web3RSVP {
         emit NewRSVP(eventId, msg.sender);
     }
 
-    function confirmAttendee(bytes32 eventId, address attendee) public {
-        // look up event from our struct using the eventId
-        CreateEvent storage myEvent = idToEvent[eventId];
+    function confirmAllAttendees(bytes32 eventId) external {
+        // look up event
+        CreateEvent memory myEvent = idToEvent[eventId];
 
-        // require that msg.sender is the owner of the event - only the host should be able to check people in
+        // make sure you require that msg.sender is the owner of the event
         require(msg.sender == myEvent.eventOwner, "NOT AUTHORIZED");
 
-        // require that attendee trying to check in actually RSVP'd
+        //confirm each attendee
+        for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+            confirmAttendee(eventId, myEvent.confirmedRSVPs[i]);
+        }
+    }
+
+    function confirmAttendee(bytes32 eventId, address attendee) public {
+        // look up event
+        CreateEvent storage myEvent = idToEvent[eventId];
+
+        // make sure you require that msg.sender is the owner of the event
+        require(msg.sender == myEvent.eventOwner, "NOT AUTHORIZED");
+
+        // require that attendee is in myEvent.confirmedRSVPs
         address rsvpConfirm;
 
-        for (uint256 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+        for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
             if (myEvent.confirmedRSVPs[i] == attendee) {
                 rsvpConfirm = myEvent.confirmedRSVPs[i];
             }
@@ -121,21 +135,21 @@ contract Web3RSVP {
 
         require(rsvpConfirm == attendee, "NO RSVP TO CONFIRM");
 
-        // require that attendee is NOT already in the claimedRSVPs list AKA make sure they haven't already checked in
+        // require that attendee is NOT in the claimedRSVPs list
         for (uint8 i = 0; i < myEvent.claimedRSVPs.length; i++) {
             require(myEvent.claimedRSVPs[i] != attendee, "ALREADY CLAIMED");
         }
 
-        // require that deposits are not already claimed by the event owner
+        // require that deposits are not already claimed
         require(myEvent.paidOut == false, "ALREADY PAID OUT");
 
-        // add the attendee to the claimedRSVPs list
+        // add them to the claimedRSVPs list
         myEvent.claimedRSVPs.push(attendee);
 
-        // sending eth back to the staker `https://solidity-by-example.org/sending-ether`
+        // sending eth back to the staker https://solidity-by-example.org/sending-ether
         (bool sent, ) = attendee.call{value: myEvent.deposit}("");
 
-        // if this fails, remove the user from the array of claimed RSVPs
+        //if this fails
         if (!sent) {
             myEvent.claimedRSVPs.pop();
         }
@@ -145,24 +159,11 @@ contract Web3RSVP {
         emit ConfirmedAttendee(eventId, attendee);
     }
 
-    function confirmAllAttendees(bytes32 eventId) external {
-        // look up event from our struct with the eventId
-        CreateEvent memory myEvent = idToEvent[eventId];
-
-        // make sure you require that msg.sender is the owner of the event
-        require(msg.sender == myEvent.eventOwner, "NOT AUTHORIZED");
-
-        // confirm each attendee in the rsvp array
-        for (uint256 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
-            confirmAttendee(eventId, myEvent.confirmedRSVPs[i]);
-        }
-    }
-
     function withdrawUnclaimedDeposits(bytes32 eventId) external {
         // look up event
         CreateEvent memory myEvent = idToEvent[eventId];
 
-        // check that the paidOut boolean still equals false AKA the money hasn't already been paid out
+        // check if already paid
         require(!myEvent.paidOut, "ALREADY PAID");
 
         // check if it's been 7 days past myEvent.eventTimestamp
